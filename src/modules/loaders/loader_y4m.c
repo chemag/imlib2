chemag/imlib2

commit c08ae5fdfba2654254a6bc040475e236676bfcf9

Y4M loader: fix C option analysis
ffmpeg has added multiple color space types that start with "420",
"422", and "444" (e.g. "420p16").

We cannot parse them as 8-bit color. Let's fail earier.

src/modules/loaders/loader_y4m.c
@@ -196,11 +196,11 @@ y4m__parse_params(Y4mParse * res, const uint8_t ** start, const uint8_t * end)
                 res->colour_space = Y4M_PARSE_CS_420MPEG2;
              else if (y4m__match("420paldv", 8, &p, end))
                 res->colour_space = Y4M_PARSE_CS_420PALDV;
-             else if (y4m__match("420", 3, &p, end))
+             else if (y4m__match("420 ", 4, &p, end))
                 res->colour_space = Y4M_PARSE_CS_420;
-             else if (y4m__match("422", 3, &p, end))
+             else if (y4m__match("422 ", 4, &p, end))
                 res->colour_space = Y4M_PARSE_CS_422;
-             else if (y4m__match("444", 3, &p, end))
+             else if (y4m__match("444 ", 4, &p, end))
                 res->colour_space = Y4M_PARSE_CS_444;
              else
                 return Y4M_PARSE_CORRUPTED;

commit cd8c225ed0aebc9f93c52314c0062adfa2d8b4a0

Y4M loader: don't fail on newline
regression introduce in c08ae5f. if the color space was the last thing
in the header than it would be followed by a newline rather than space.

peek to ensure next char is either ' ' or newline to avoid falsly
matching problematic colorspaces. this will also work for other cases
like "mono-whatever" instead of working just for "420", "422" and "444".

also change the return code to unsupported instead of corrupted in these
cases.

src/modules/loaders/loader_y4m.c
@@ -38,6 +38,7 @@ typedef struct {
    ptrdiff_t           w, h;
    ptrdiff_t           fps_num, fps_den;
    enum {
+      Y4M_PARSE_CS_UNSUPPORTED = -1,
       Y4M_PARSE_CS_420,         /* default picked from ffmpeg */
       Y4M_PARSE_CS_420JPEG,
       Y4M_PARSE_CS_420MPEG2,
@@ -113,6 +114,7 @@ static enum Y4mParseStatus
 y4m__parse_params(Y4mParse * res, const uint8_t ** start, const uint8_t * end)
 {
    const uint8_t      *p = *start;
+   const uint8_t      *peek;
 
    for (;;)
      {
@@ -173,6 +175,7 @@ y4m__parse_params(Y4mParse * res, const uint8_t ** start, const uint8_t * end)
              }
              break;
           case 'C':
+             res->colour_space = Y4M_PARSE_CS_UNSUPPORTED;
              if (y4m__match("mono", 4, &p, end))
                 res->colour_space = Y4M_PARSE_CS_MONO;
              else if (y4m__match("420jpeg", 7, &p, end))
@@ -181,20 +184,26 @@ y4m__parse_params(Y4mParse * res, const uint8_t ** start, const uint8_t * end)
                 res->colour_space = Y4M_PARSE_CS_420MPEG2;
              else if (y4m__match("420paldv", 8, &p, end))
                 res->colour_space = Y4M_PARSE_CS_420PALDV;
-             else if (y4m__match("420 ", 4, &p, end))
+             else if (y4m__match("420", 3, &p, end))
                 res->colour_space = Y4M_PARSE_CS_420;
-             else if (y4m__match("422 ", 4, &p, end))
+             else if (y4m__match("422", 3, &p, end))
                 res->colour_space = Y4M_PARSE_CS_422;
-             else if (y4m__match("444 ", 4, &p, end))
+             else if (y4m__match("444", 3, &p, end))
                 res->colour_space = Y4M_PARSE_CS_444;
-             else {
+
+             peek = p; /* peek to avoid falsly matching things like "420p16" */
+             if (res->colour_space == Y4M_PARSE_CS_UNSUPPORTED ||
+                 (!y4m__match(" ", 1, &peek, end) &&
+                  !y4m__match("\n", 1, &peek, end)))
+             {
 #if IMLIB2_DEBUG
                 char str[1024];
                 sscanf(pp, "%s", str);
                 D("%s: unknown color type: '%s'\n", __func__, str);
 #endif
-                return Y4M_PARSE_CORRUPTED;
+                return Y4M_PARSE_UNSUPPORTED;
              }
+
              break;
           case 'A':
              if (y4m__match("0:0", 3, &p, end))

test/images/icon-64.framerate_1_1.y4m
@@ -1,3 +1,3 @@
-YUV4MPEG2 W64 H64 F1:1 Ip A368:375 C444 XYSCSS=444 XCOLORRANGE=LIMITED
+YUV4MPEG2 W64 H64 F1:1 Ip A368:375 XCOLORRANGE=LIMITED XYSCSS=444 C444
 FRAME
 λλλλλλλλλλλλλλλλλλλλλλλλλλ»—h]]]\[‹ΊÒλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλΗ‚_DIJHHGFEDA=83/Cq΅ίλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλΣƒ_]jijmpw}zvqjaWLB80*d­λλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλΣ~^z|~ƒ” ®ΉΑΕΕΓΎΉ―£“n\L>3+?΅λλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλί|e‰‘©½ΝΤΧΧΧΧΧΧΧΦΦΥΡΗ³•zaN=1)>Ήλλλλλλλλλλλλλλλλλλλλλλλλλλλλλc“–΅―ΔΥΨΧΧΧΦΦΦΦΦΦΦΦΦΦΦΦΠ¶‘rYE6,&cίλλλλλλλλλλλλλλλλλλλλλλλλλΰu€› §ª¶ΝΩΨΤΠΞΝΜΜΛΛΜΜΝΞΞΡΤΦΦΥ½y^J9.'1ΉλλλλλλλλλλλλλλλλλλλλλλλΙd΅£¥ª­²ΑΞΟΙΕΒΑΑΐΏΏΏΏΐΑΒΑΒΖΙΞΡΙ―’v]I9.'%”λλλλλλλλλλλλλλλλλλλλλΙhª¨¨©ª«ª°¶»Ί·¶¶µµµ΄΄΄΄µµ¶¶΄¶Έ»½Ό°›ƒlWE6,%%oλλλλλλλλλλλλλλλλλλλΚr°―ª¬ª¨Ά£¦ª¬ª©©ªªª©©©©©ªªª«©¨ªª«ª¥™‡saO?2)$%oλλλλλλλλλλλλλλλλλΚq΄΄­®ª¦™ ››—„udTE8.'$%oλλλλλλλλλλλλλλλΚhµ·±°¬¦ “‘‘‘’’‘““””••••••••••’„|qdVH<2*%$%”λλλλλλλλλλλλλΰo°Ί΄³­¦“‡††‡†‡‰‰‡‡‡†††‡‡‡‡„„ƒ€~zsj`UI>4,&$$%­λλλλλλλλλλλλzΎµ·°§ —†~|{{~|{{z{||}}}}}}}}||{zxvsojbZQG>5.($$$1ÒλλλλλλλλλλΉ~Α·Έµª΅™~xtpoppnopstuuvvvvvvvvuttrpnkgb\TME<5.($$$%WλλλλλλλλλαhΎ»·Ή®¤‘}uojc`cghhjlmnopppppppponlkjgd`\VPIA:3-($$$$%΅λλλλλλλλ™Β¶Ό΄¨“†tmbXW[_bdbdfhijjkkkkjjjihgeda_[VRLF@92,($$$$$2ίλλλλλλΨsΗΊΊΈ― –‹zjWOQSWZ^`^_abddefffffeedcb`^\YVSNID>93-($$$$$%λλλλλλ§ΔΈ½³¨…iNLMNPTWZ]\[]^`aabbbbaaa`_^\ZXVROKGB=83/*'%$$$%3ίλλλλβ}ΙΌ½Έ®΅“‰qKJKKLNQTWYZXZ[\]]^^^^^^]\[ZXWURPLIEA=840+*(&%$$%΅λλλλΈ¥Θ»Ώ³¨›pZJJIJJLORTWYVWXYZ[[[\[[[[ZYWVTRPNJGDA<851.,+)'&$%?λλλλΑΑΏΊ―£–s_XJJIIIKNPSUVVTVWXXYYYYYYXWVUTRPNLIFC@<9620/-,*('%'ΖλλÒΝΌΒ·«|d_VKJIHHJMORSUWSTUVWWXXXXWWVUTRQOMKIFC@=;75320.,+)''}λλΑ―Κ½Ώ΄§le_WKKJIIKMPQTUWVTUUVWWWWWWVUUTRQOMKIFDB?<:86531/.,*(Mλλ©ΎΓΏ»°uplf]\\\]_acefhhjgghhiiiiiihhgfedca_^\YVSQOMKIGDB>:50+ίλΗΑΕΏµ›“†ƒ}}}~€‚„†‡‰‰‹‹‹‹‹‹‰‡†…ƒ‚€}zxusomjhe`[SI>2®Ò±ΡΚΣΠΔΐΎ»ΊΊ»ΊΌΏΑΔΝΥΥΦΦΦΦΦΧΧΦΦΣΠΞΝΜΜΛΝÒΥΤΜΔΒΐΎΌΊΈµ³°­«¨¦Ά“…r[AΣµΛΎΒ²—‰ƒ€yyyyz|}‚ƒƒ……†…ƒƒ„„„„ƒ΅ΆΆΆΆ›}|zywtrpmkhfc`[ULB7h»ΈΗΊΊ”yrongddcdeghijlmmnopnlmmmmn‹‹‹‹‹xfedba_][YWXTPMJFB<4\ΔΌΖ½µ¤•‡~}ywxxyz|}~€€‚‚ƒƒ€€€€†•••••––{zywvtqnlinlic\UNF=;ΑΑΛΘΓΐΏΌΉ¶΄΄¶¶¶ΈΊΌΎΐΓΔΕΕΕΕΖΖΖΖΖΕΓΒΑΓΗΗΖΖΖΕΕΒΊΉΈ¶΄±―¬ª§¨¦£zgQ6«ΑΗ½»·³―ª¦ΆΆΆ£΅Ά£¥§©«¬­­­®®®――――­¬««°±±±°°°©¦¤¤Ά ›–“”“†~wbL5¬Ώΐª­¤’‹ƒ~zyysrrsuwxyz{|}}~~€~||||€‚‚‚‚|xwvtsqomkhfiigea_chF1ΐΏΊ¦¬£‘‰~wwvspprsuwxyzz{|}}~~~~{z{zz{zzxwvutsqpmkigdggfc`biqc1ΑΑ»­·±«¥›™•”––””–›     ΅΅ ››™–”’‰†‡‡…~ƒƒ?ΉΔΓΎΝΞΞΞΜΜΜΝΞΠÒΤΥΧΩΪΪΫάέέέέέέέέέέήέΫΪΪΩΩΩΨΧΧΦΥΤΣΡΠΝΛΙΗΕΔΐΎΏΌ―–hΞΏΌ¨µ±ª¤––‘’““’’”–—™›››››››™–•”’‹‰†ƒ‡“™‘nΥ²½Ά©¦•†‚€|yyy{wxz{}~€‚ƒƒ„„„„„ƒ€€€|{yxvtrpnljs†‰”•‡λ¥ΐ¨§«£›”‰‡‚‚ƒ„†„„†‰‹‹‹‡†…‚|zxus~’—w²λ°Γ¶±½ΊΉ¶³±±²³³µ·ΊΌΌΎΐΑΒΔΕΘΘΙΙΛΚΙΚΚΘΕΑΏΎΎΏΎ»Έ¶΄²®«¨¥΅ §―°°³¶®§dΰλΐΒΊ¬¶·³°¬©¨¨©§¨«­―±±±³µ¶ΈΉΊΓΞΝΛΕΌ»»Ήµ΄³µ΄³°­«©§¤΅¤¬®°³Έ―«iλλή°Ί΅΅¥—‹‡‡ƒ„‡‰‹‘’”©ΘΕΓ°—––•“‘‘“’†„‚€~{ywy”™΅ª΄¨”‡λλλ­Ύ¤Ά|{||wwy{}€ƒ…ƒ„†‡‰ΏΩΖ”‰†‰‰‡…ƒzwusqomkhl‡™£―ΉªkΚλλλΐΌ­™›’‰‚|{{{|xy|€ƒ†‰‘‘‘‘‹‹†~yvsqoljjo– ¬ΉΊ©eλλλλε¥Ή›“›”‹ƒ}|||}|€ƒ†‰‘“’‘“”•––—––•“““‘„}zvsqolnx•©µΑΈªλλλλλ±»¥•“•…}}}‚„ƒ‡“•—™™™››››΅’’‹}yurpns…§²ΎΘ±Yΰλλλλλί£³•“†€~~…‰‹‘”—™  ΅     ¦·³±―­¦›’…}xurrw”¤―»ΖΓ†–λλλλλλλ·³΅‹‚ƒ‡‹’’•› Ά¤¥§¨§¥¥¥¥ΐΖΓΒΐΏΎ©‰…€|wswΆ¬ΈΔΞ³dΰλλλλλλλε―‘‹‰ƒ€€†‰“— ΅¤§¨ª¬¬­®¬«¬ΠΣÒΠΠΞΝ¶„~yx|ª¶ΐΜΗzΈλλλλλλλλλΠ–‹…†„€€ƒ‡‘– £¦©«­―°±²²΄¶΄ΞίήέάΫΩΈ ‘‡|}΅³½ΙΡ¤sλλλλλλλλλλλΉ’vw{€€‚‡”¥¨ª«­―±²΄¶ΈΊΌΌ½ΎΫθηζβΘΎ•‰„°»ΖΡΏmαλλλλλλλλλλλζ®‘ilpuz~„•› ¥¬°²΄¶ΈΉΌ½ΏΐΒΓΕΔΓΚΡΠΕΕΏ’†…‡¤ΈΓΞΛ€Δλλλλλλλλλλλλλήªjpsv{„’£¨­³¶ΈΊΌΎΐΒΕΖΘΙΙΚΛΚΛΛΛΜ³΅›•‹“¶ΑΜΟ–ªλλλλλλλλλλλλλλλί°lrvy€‡—¥«°΄ΉΌΎΐΒΕΗΙΛΝΞΟΠΡΡΡΡΡΚª¤–‘®ΏΙΞΆ‘λλλλλλλλλλλλλλλλλΰµ™ouy†–¥¬±¶ΊΏΓΖΘΚΜΟΡÒΤΤΦΦΧΦΦΣ¶­¦™“ΆΌΖΜ¦•λλλλλλλλλλλλλλλλλλλα»¨uv}„•¦­΄ΊΎΑΗΛΞΠΣΤΦΨΩΩΪΫΫΫΦΏ·°¨ ’›ΊΔΘ΅•λλλλλλλλλλλλλλλλλλλλλβΕΈv~‰’›¤­µ½ΒΕΗΛΡΥΧΩΫάέήήίήΣΕΐΉ²©΅—™·Βΐ•ΈλλλλλλλλλλλλλλλλλλλλλλλθΞΔy•©³½ΔΚΜΝΞΡΦΩέίίίΫΦΝΛΘΓ»±§›µΎµ”ΡλλλλλλλλλλλλλλλλλλλλλλλλλλΪΛ·‡“ª¶ΒΜÒÒÒÒÒÒΣΣΣÒΡΡΡΡΞΔ·ª΄µ΅¥βλλλλλλλλλλλλλλλλλλλλλλλλλλλλιΦΝ­‰‹”©µΒΞΤΤΤΤΥΥΥΥΥΤΤΤÒΖ·ª¦®©ΛλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλζΧΝ²”’›¥―ΊΕΞÒΥΦΧΧΧΦΤΡΘΌ±¦ ©¦ΎλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλδΨΡΓ­›¥¬΄»ΐΓΖΖΕΒ½µ®¨Ά¥›΄ΝλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλίΠΜ½Όµ®ª¨ª¬¬«­―«΅¤΄ΐδλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλλΥΛΞΗΐΏ½ΈµΕΊΠήλλλλλλλλλλλλλλλλλλλλλλλλλ€€€€€€€€€€€€€€€€€€€€€€€€€€„‰‰„‚€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€ƒ““”“’‘‘‘‘‘‘’’‹†€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚‰‘‘’’‘‘’’‘‹…€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚†……„…„…†‰‹‘’‘†€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‰‡„ƒ‚‚‚‚‚‚‚‚‚‚‚ƒ†‰‘’‘„€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‡‹†ƒ‚‚‚‚‚‚‚‚‚‚‚‚‚‚‚‚ƒ†‰‘’‘€€€€€€€€€€€€€€€€€€€€€€€€€‹‹‰‹‰…‚‚ƒƒƒƒ„„„„„„„ƒƒ‚‚‚‚‚„‰’’„€€€€€€€€€€€€€€€€€€€€€€€ƒ‰‰‹‹‰‡„„„…………†…††††……„„„ƒ‚‚ƒ‡‰‘’’†€€€€€€€€€€€€€€€€€€€€€ƒ‰‹‰‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡††……………†‹‘’’‰€€€€€€€€€€€€€€€€€€€ƒ‰‹‹‰‰‰‰‰‰‰‡‡‡‡‡‰‹‘’’‰€€€€€€€€€€€€€€€€€ƒ‡‰‹‹‹‰‰‰‰‰‹‘‘’‘‰€€€€€€€€€€€€€€€ƒ‡‰‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‘‘’’†€€€€€€€€€€€€€‡‡‰‹‹‹‹‹‹‹‹‹‹‹‹‹‘‘’’‘…€€€€€€€€€€€€‰‡‰‹‘‘‘‘‘‘’’‘‚€€€€€€€€€€…†‰‘’’““’‘‘’’’‘‹€€€€€€€€€†‡‰‹‘’““““’‘‘‘‘‘’’’’†€€€€€€€€‰‹†’“””“’’’’’‘‘‘‘‘’’’’’’’€€€€€€‚†‰‹’“””“”“““““’‘‘‘‘‘‘‘‘‘‘‘‘‘‘’‘’’’’’’’’’’““’’‡€€€€€€‡‘““”””””””””“’’’’’’’’’’’’’’’““““““““““”“““’€€€€†‰’”””••••••••””“““”““”””“““““””””””””””””““‘†€€€€†‹†”•”••––•––––••”””•””””””•”•”•”••””•••••””“‘‘€€€€‰’”•”•––—–––———––•–––•––––•––––––•–––––•••”’’‘‘‘ƒ€€ƒ†’“””••–—————–———–————–———————–—––––••”’’’‘‘‘‰€€…†‰‹‘“”••”–—————————————————————————–––”“““’’’’‘‘€€‰‡’’““”“”••–•––––––––••––•––––––••–•–––••••”““““’’’‘‘‘€‹‡‡‰‹‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘…ƒ‰…†…†‡‡‡‡††…„„„„„„„„„„„„„„…………†…„„„…‡‡‡‡‡‡‡‡‡‰‰‡ƒ‰‡‰‰‹‘‘‘’‘‘’’’’’’“““““““““““““““‘’’’‘‘‘‘‘‘‘‘‘‘‘†‰‡‹’“””•••••––––––————’’’’‘‘‘“–––••••••”•””““’’’‘‹…‰‹‘’’’“““’““““””””””””•••”””””“““““““““’’””““’’’’‘†‡†‡‡‰‰‰‰‰‰‰‰‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‰‰‰‰‰‰‰‰‹‹‹‰‡‰‰‹‹‘‘‘‰‰‘“”•••–••–––––——————————––———–•••••–—–––––––•••––•––••”“†‡’”•–––——–—————————————–——————————————––——–—––•”’†‡‰‹‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘‘’’’’’””‘‡†‡††††‡‡‡†††……„„„„ƒƒƒ‚ƒƒƒƒƒƒƒƒƒƒ‚ƒƒ„„ƒ„„„„…………†…††††‡‡‹‹„‡‰‹‘’““““““’’’’’‘‘‘‘‘‘‘‘‘‘‘‘‘‘’’’““““““”•—––“ƒ‰‘“•–—™™™™™——————––––––––––••––––——————™›”€‹‘“”–—–————–––•••”””””“””“”“”“““”“”•”•”••––————™•’…€‰‡‹‹‹‹‰‰‰‡‡‡‡‡‡‡‡‰‰‰‹‹‹‹‘’‘‘‘€‡‡‰‹‹‹‹‹‹‰††‡‹‹‹‹‹‹‘’“’‘‹€€‚‰’”–———––••”””““‰’““““““”“”•”•••–––——–“‰€€€‘‘“–››™—––––•„“•••––•–––——™™™›™–‘ƒ€€€‡‰‘’“–››™™——––––•••••••••••––——™™™››—“€€€€““–›››™——–•••”””““““““””••––––——™™™››™•‹‹†€€€€€‹‰“”•›—––•”””““’“’’’““’’’““••––—™™››™–’‰€€€€€‚‹“•–›››—–••”””““’’’’’’’‘”•––—™™™–’‹‰‹€€€€€€€•—›››™—–••”““’’‘‘‘‘‘‹‰‹‹“•––—™™—”€€€€€€€”—™™›››—––””““’‘‘‡†††‡‡‡’”•–—™™•‘‰…€€€€€€€€€…‘‘—™››››™––•”“’’‘ƒƒ„„„…‘””––—–’‡€€€€€€€€€€€‰‘››››™—–•”“’‘„‚‰‘“”•–——“‹‡€€€€€€€€€€€‘››™–•”“’‘‹‹‹‰‡‡‰‹‘’“•–——”‡„€€€€€€€€€€€€€ƒ’›–•”’‘‹‹‰‰‰‰’“”–—•‰‡‹‡€€€€€€€€€€€€€€€‚“™—–•“’‹‹‰‰‡‡‡††‘“”––’‡‹€€€€€€€€€€€€€€€€€‚‹‘™™—–”“‘‹‰‡‡‡††††…†‘“•–“‹‰€€€€€€€€€€€€€€€€€€€‚™™–•“‘‹‰‰‡‡†…………„„„…‘“•”‹‰‹€€€€€€€€€€€€€€€€€€€€€‚•–•“‘‹‰‰‡††…„„„„ƒƒƒ†‰‹’””‹†€€€€€€€€€€€€€€€€€€€€€€€†‘——•”’‹‰‡‡‡‡……„„ƒƒ„…‡‡‰‹‘“”ƒ€€€€€€€€€€€€€€€€€€€€€€€€€€ƒ†‹•–•“’‡†††…†††……†††††‡‰’“‰€€€€€€€€€€€€€€€€€€€€€€€€€€€€€„†••“‘‰‡…………†…………………†‰’“‘‹„€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€„†““’‹‰‡………„…………†‹‘’’†€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚„…’’‹‰‰‰‰‹‘‘„€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€ƒ††‰†€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€…††‡‰‰‰†…‚€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚ƒ„………………„ƒ‚€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€„…‡‡‡‡‡‡‡‡‡‡‡‡‡†„‚€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€„…†††‡†††††………†††‡‡‡‡‡†„€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€„††††………„ƒ‚‚‚‚‚‚‚ƒƒ„…††‡‡‡‡…‚€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€ƒ………………ƒ‚€‚„…†‡‡‡†…€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚„„„……„ƒ‚€€€€€‚„†‡‡‡‡†„€€€€€€€€€€€€€€€€€€€€€€€€€€€ƒ„ƒ„„„„ƒ€‚„…‡‡‡‡†…€€€€€€€€€€€€€€€€€€€€€€€„ƒƒ„„„„ƒ‚‚‚‚‚‚‚‚‚‚‚‚‚‚‚€ƒ„…†‡‡‡†…‚€€€€€€€€€€€€€€€€€€€€€„‚ƒƒ„„„„ƒƒƒ‚ƒ‚ƒ‚ƒƒƒƒƒƒƒƒƒƒ‚‚‚‚‚‚ƒ„…†‡‡‡‡†…ƒ€€€€€€€€€€€€€€€€€€€„‚ƒƒ„„„„„„ƒƒƒƒ„ƒƒƒ„„„„„ƒƒƒƒƒƒƒƒƒƒƒ„„††‡‡‡……ƒ€€€€€€€€€€€€€€€€€„‚‚ƒ„„„„……„„„„„„„„„„„„„„„„„„„„„„„„ƒ„……†‡‡†……ƒ€€€€€€€€€€€€€€€„‚‚ƒƒ„„„…†………„……„„„„„„„„„„„………„„„„„………††‡‡‡‡‡†……‚€€€€€€€€€€€€€€„‚‚ƒƒ„„………†……†††………………„………„„……„……„……………††‡‡‡‡‡†………€€€€€€€€€€€€„ƒ‚‚ƒƒ„„…††††††††…………………………………†……………†…†††‡‡‡‡‡‡†…………€€€€€€€€€€‚„‚‚ƒ„„…††‡†‡‡‡††††††††††††††††††††††††‡†‡‡‡‡‡‡‡…………„€€€€€€€€€…‚‚‚‚ƒ„………‡†‡‡‡‡‡‡‡†‡‡††‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡†…………‚€€€€€€€€ƒƒ‚‚‚ƒƒ„…†††‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡………………€€€€€€€…‚‚‚ƒ„……†‡‡‡‡………………ƒ€€€€€€ƒƒ‚‚‚‚ƒ„…††‡‰‰‰‰‰‰‰‰‰‰‰‰‰‰‡…………………€€€€€„‚‚‚‚ƒƒ„…†‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰†††……………‚€€€€‚ƒ‚‚‚ƒ„„†‡‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰†††††…………€€€€„‚‚‚‚ƒ„„…†‡‰‰‰‰‰‰‰‰‰‡‡†††††………€€ƒ‚‚‚ƒƒ„…††‡‰‰‹‹‹‹‰‰‰‡‡‡‡††††……ƒ€€‚ƒ‚‚‚ƒ„…††‡‡‰‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‡‡‡‡†††…„€€ƒ‚ƒ‚ƒƒ„…†‡‡‡‡‰‰‰‰‰‹‹‹‹‰‰‰‡‡‡‡††††…€€ƒ‚‚ƒƒƒ„………†……††‡‡‡‡‡‡‡‡‡‡‡‡‡††††††††††††……‚ƒ‚‚‚‚‚‚ƒƒ‚‚ƒ‚‚‚‚‚‚‚‚‚‚‚‚ƒƒƒ‚‚‚ƒƒƒƒƒƒƒƒƒƒƒƒƒƒ„„ƒ„„……ƒƒ‚ƒƒ„„…††††‡†‡‡‡‡‡‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰†‡††††‡‡‡‡‡‡‡†‡††††ƒ‚ƒƒƒƒ„…†‡‡‰‰‰‰‰‰‹‹‹‹‹‹‹‹‹‹‰‰‰‡‰‰‰‰‰‰‰‰‰‡‡‡†„‚‚‚ƒƒ„……†‡‡‡‡‡‰‰‰‰‰‰‰‰‰‡‡‡‡‡‡‡‰‰‰‰‰‡‡‡‡†„‚‚‚‚‚ƒƒƒƒ„ƒ„„ƒ„ƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒ„„„ƒƒƒƒƒƒƒƒ„„„„„„„„„„„„„„………††…ƒ‚‚ƒƒƒ„„„„………………………†……………†……†…………†††…†††………†††††††††††††††††††‡…ƒ‚ƒ„„……†‡‰‰‰‰‹‹‰‰‰‰‰‰‰‰‰‡‡…‚ƒƒ„„…†‡‡‰‰‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‰‰‡‡…‚‚ƒ„ƒ„……††‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡†……ƒ‚‚‚‚‚‚‚‚‚ƒƒ‚‚‚‚‚‚‚‚‚‚‚‚‚‚‚‚‚‚ƒ‚ƒƒƒƒƒƒ„…„…„„‚ƒ„„„…††‡‡‡‡‡‡‡‡‰‰‰‰‰‡‡†„„ƒƒ………†‡‰‰‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‰‰‡†„‚€„ƒ………†‡‡‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‹‹‹‹‰‰‰‡†„€ƒ‚ƒ„„„„………††…………………„„„„„„ƒƒƒƒƒƒƒƒƒƒƒ„„„„„„………………†††‡‡†‡†††……„„€€‚‚ƒ„„„……††‡‡‡‡‡††††………………„„„ƒƒƒ„………„………………††††‡†‡‡‡‡‡‡‡‡‡†……„„€€€ƒƒ†††‡‰‰‹‹‹‰‰‰‰‰‰‰‰†ƒ„„†‰‰‰‰‰‰‰‰‰‰‰‰‹‹‹‰‰‡……„ƒ€€€„ƒ†††‡‰‰‹‹‹‹‹‹‹‹‹‰…‚„‰‰‰‹‹‹‹‹‹‹‰‡……„€€€ƒ‚„†‡‡‰‹‹‹‹‹‹‹‰‰‰‰‰‰‰‰‰‹‹‹‹‹‹†…„„€€€€€„ƒ†‡‰‰‹‹‹‹‹‰‰‰‰‰‰‰‰‰‰‰‰‰‰‰‹‹‹‹‹‹‹‰‡……„‚€€€€€ƒƒ†‡‰‹‹‹‹‰‰‰‰‰‰‰‰‰‰‹‹‹‹‰‡…„„„€€€€€€„„‡‰‰‹‹‹‹‹‹‹‰‰‰‡††††‡‰‹‹‹‰‡†„„„ƒ€€€€€€€ƒ„†‰‹‹‹‹‹‹‹‰‰‰‰…„„…………‡‰‹‹‹‹‰‡„ƒ„„€€€€€€€€…„‹‹‹‹‹‹‹‰‰‰‡‡‡‡‡‡‡‡ƒƒƒƒƒƒƒ†‹‹‹†…„„„‚€€€€€€€€€‚…†‹‹‹‹‹‹‹‰‰‰‡‡‡‡‡†††††ƒ‚‚‚†‰‹‰‡†„ƒ„„€€€€€€€€€€€ƒ†‹‹‹‹‹‹‹‰‰‡‡‡††††††††…‚„…‰‰‹‡†„ƒ„…€€€€€€€€€€€€€ƒ†‹‹‹‰‰‡‡†††††……………………„ƒƒ„„…‰‰†…ƒƒ„€€€€€€€€€€€€€„‡‹‹‹‹‰‡‡†‡†……………„…„„„„„„„„ƒ†‰‰‰‡…„ƒ„ƒ€€€€€€€€€€€€€€€„‡‹‹‹‹‹‰‡‡†††…………„„„„„ƒƒƒƒƒƒƒ„‡‰‰‡…„ƒ„ƒ€€€€€€€€€€€€€€€€€„†‹‹‹‹‡††……„„„„„„ƒƒ‚ƒ‚‚‚‚ƒ‚††‡‰‰‰…„„„ƒ€€€€€€€€€€€€€€€€€€€ƒ…‹‹‹‰‰‡††……„„„ƒƒƒƒ‚‚‚‚‚‚‚…††‡‰‰†„„„ƒ€€€€€€€€€€€€€€€€€€€€€‚„‰‹‰‰‡†……„ƒ„ƒƒ‚‚‚‚‚‚ƒ„…††‡‰‰†…„…‚€€€€€€€€€€€€€€€€€€€€€€€€‚‚‰‡†…„ƒƒƒƒƒ‚‚‚‚‚ƒƒƒƒ„…†‡‰†……„€€€€€€€€€€€€€€€€€€€€€€€€€€‚„‰‰‰‡†„ƒƒƒƒ‚‚‚‚‚‚‚‚‚‚‚ƒ„†‡††…ƒ€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚…‰‰‰‡†„ƒƒ‚‚‚‚‚‚‚‚‚‚‚‚„†‡‡…„€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚…‰‰‡†…„ƒ‚‚‚‚‚‚‚ƒ…†‡†„‚€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚ƒ†‡‡†……„„„„„……†‡‡†…ƒ€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚‚ƒ„††‡‡‡†‡†††††…ƒ‚€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€‚‚‚ƒ‚ƒƒƒ‚ƒ€€€€€€€€€€€€€€€€€€€€€€€€€€